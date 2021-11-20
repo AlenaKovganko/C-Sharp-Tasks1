740fe6aff5dab6fa109ebff16fab59779bb78162
Пользователь <alena_kovganko@mail.ru>
2021-11-20 22:02:30 +0300

    1

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трезначное число");
+int number777 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вторая цифра трехзначного числа");
+Console.WriteLine(number777%100/10);
---
+++ Task20/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ввести номер четверти, показать диапазоны для возможных координат
+
+string Range(int q)
+{
+    string result = String.Empty;
+    if(q == 1) result = "Диапазоны для возможных координат: x>0 & y>0 ";
+    if(q == 2) result = "Диапазоны для возможных координат: x<0 & y>0";
+    if(q == 3) result = "Диапазоны для возможных координат: x<0 & y<0 ";
+    if(q == 4) result = "Диапазоны для возможных координат: x>0 & y<0";
+    return result;
+
+}
+
+Console.WriteLine("Введите № четверти");
+int q = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Range(q));
