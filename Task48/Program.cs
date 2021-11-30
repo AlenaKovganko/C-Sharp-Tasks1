e79362235405cbcdb3e8681330719c30dc1e455a
Пользователь <alena_kovganko@mail.ru>
2021-11-30 21:41:19 +0300

    48

---
+++ Task48/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+Random rnd = new Random();
+int m = 5;
+int n = 4;
+int [,] array = new int [m,n];
+void CreateArray(int [,] array)
+{
+    for(int i =0; i <array.GetLength(0); i++ )
+    {
+        for(int j=0; j <array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(1,100);
+        }
+    }
+}
+void PrintArray(int [,] array)
+{
+    for(int i =0; i <array.GetLength(0); i++ )
+    {
+        for(int j=0; j <array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i,j],4}");
+        }
+    Console.WriteLine();
+    }
+
+}
+Console.WriteLine("Рандомный массив");
+CreateArray(array);
+PrintArray(array);
