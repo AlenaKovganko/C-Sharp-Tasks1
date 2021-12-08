ebe9132ab32b492c4e6cd5480b44bc7f0cd2ae99
Пользователь <alena_kovganko@mail.ru>
2021-12-08 22:40:34 +0300

    66

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+int n = Convert.ToInt32(Console.ReadLine());
+int NaturalNumbers(int n)
+    {
+        if (n==1) return 1;
+        else return  NaturalNumbers(1)*n;
+    }
+for (int i=1; i<=n; i++)
+{
+    Console.Write(NaturalNumbers(i) + " ");
+}
+
+NaturalNumbers(n);
+
+
+
